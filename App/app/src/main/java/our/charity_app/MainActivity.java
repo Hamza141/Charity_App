ccce307e530fa41218106366140a61fc99716de5
Muhammad Hamza Farrukh <mfarrukh@purdue.edu>
2016-10-27 20:36:46 -0400

    Buttons now lead to other Activities and display proper labels

--- App/app/src/main/java/our/charity_app/MainActivity.java
+++ App/app/src/main/java/our/charity_app/MainActivity.java
@@ -1,13 +1,70 @@
 package our.charity_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    //Declare buttons
+    Button about;
+    Button donations;
+    Button list;
+    Button vote;
+    Button watch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initializing buttons
+        about = (Button) findViewById(R.id.about);
+        donations = (Button) findViewById(R.id.donations);
+        list = (Button) findViewById(R.id.list);
+        vote = (Button) findViewById(R.id.vote);
+        watch = (Button) findViewById(R.id.watch);
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        donations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DonationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        vote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, VoteActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        watch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, WatchActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
